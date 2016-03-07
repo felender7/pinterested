534ad1ef051f268bd6bffe00e1b47857abf87c39
Felender <felender7@gmail.com>
2016-03-07 19:36:02 +0200

    associate pins with user

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,16 +1,19 @@
 class PinsController < ApplicationController
   before_action :set_pins, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show] #check if the user has signed in?
+  before_action :correct_user, only: [:edit, :update, :destroy]
   def index
     @pins = Pin.all.order("created_at DESC")
   end
 
  def create
-   @pin = Pin.new(pin_params)
-    if @pin.save
-      redirect_to @pin,notice:'Pin was sucessfully created'
-    else
-       render'new'
-    end
+   #@pin = Pin.new(pin_params)
+     @pin = current_user.pins.build(pin_params)
+      if @pin.save
+        redirect_to @pin,notice:'Pin was sucessfully created'
+      else
+         render'new'
+      end
  end
 
   def show
@@ -20,15 +23,15 @@ class PinsController < ApplicationController
  end
 
  def update
-     if @pin.update(pin_params)
-        redirect_to @pin, notice:'Pin was sucessfully updated'
-     else
-        render 'edit'
-     end
+       if @pin.update(pin_params)
+          redirect_to @pin, notice:'Pin was sucessfully updated'
+       else
+          render 'edit'
+       end
  end
 
   def new
-    @pin = Pin.new
+    @pin = current_user.pins.build
   end
 
 
@@ -36,11 +39,16 @@ class PinsController < ApplicationController
    @pin.destroy
     redirect_to @pin, notice:'Pin was secessfully deleted'
  end
+
+  private
+        def set_pins
+            @pin = Pin.find(params[:id])
+        end
+        def pin_params
+            params.require(:pin).permit(:description)
+        end
 end
-private
-    def set_pins
-        @pin = Pin.find(params[:id])
-    end
-    def pin_params
-        params.require(:pin).permit(:description)
+    def correct_user # check if the user is authorised to edit,update or destroy the pin
+      @pin = current_user.pins.find_by(id: params[:id])
+          redirect_t pins_path, notice: "Not authorised to edit this pin" if @pin.nil?
     end
