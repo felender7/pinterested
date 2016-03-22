c053852e3a8917b3d6b3c79d0e8b88967129168d
Felender <felender7@gmail.com>
2016-03-22 12:08:56 +0200

    add avator to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          has_many :pins, dependent: :destroy
-         has_attached_file :avator, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-         validates_attachment_content_type :avator, :content_type => /\Aimage\/.*\Z/
+         has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }
+         validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
