6e311317fc5e07b29261ac1416a4535b499ddd1c
Felender <felender7@gmail.com>
2016-03-22 10:15:12 +0200

    removed add_attachedment avator to users migration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :avator
-    devise_parameter_sanitizer.for(:account_update) << :avator
+    devise_parameter_sanitizer.for(:sign_up) << :name
+    devise_parameter_sanitizer.for(:account_update) << :name
   end
 end
