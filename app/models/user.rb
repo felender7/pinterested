ab40196da390370dbfff91d8935fb8da412b3a6e
Felender <felender7@gmail.com>
2016-03-30 10:40:05 +0200

    modified user_module

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          has_many :pins, dependent: :destroy
-         has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }
+         has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" },
+         :storage => :dropbox,
+         :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+         :dropbox_options => {:path => proc { |style| "#{style}/#{id}_#{image.original_filename}"} }
          validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
