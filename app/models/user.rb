72e6f6544f7cde579b8ff4f8e4bbd2d48199f888
Felender <felender7@gmail.com>
2016-04-06 12:16:30 +0200

    fixed user_model for user avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,6 @@ class User < ActiveRecord::Base
          has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" },
          :storage => :dropbox,
          :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
-         :dropbox_options => {:path => proc { |style| "#{style}/#{id}_#{image.original_filename}"} }
+         :dropbox_options => {:path => proc { |style| "#{style}/#{id}_#{avatar.original_filename}"} }
          validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
